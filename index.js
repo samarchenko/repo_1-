879124b8f26cbf6afc7d6e17f7506ac6dd402cb7
samarchenko <anton.samarchenko6@gmail.com>
2023-02-15 00:55:23 +0200

    file html, css, js - under the name index2, this is a calculator

---
+++ index.js
@@ -0,0 +1,13 @@
+let a = 5; 
+let b = 10 ;
+function q (){
+    if (a  == 5,b == 10){
+        let c = a + b; 
+        alert('answer: ' + c + ', I understand!');
+
+    }
+    else {
+        alert('i dont understand!'); 
+    }; 
+};
+q();
