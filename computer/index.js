c2331640f62f8858e4812de7fc3635c01f2ad7ac
samarchenko <anton.samarchenko6@gmail.com>
2023-03-25 18:48:57 +0200

    site what is a computer

---
+++ computer/index.js
@@ -0,0 +1,14 @@
+let a = 5; 
+let b = 10 ;
+function q (){
+    if (a  == 5,b == 10){
+        let c = a + b; 
+        alert('answer: ' + c + ', I understand!');
+
+    }
+    else {
+        alert('i dont understand!'); 
+    }; 
+};
+q();
+alert ('hi Anton!'); 
