769f27ea8c40b231667adc1663a28b3f95195713
samarchenko <anton.samarchenko6@gmail.com>
2023-04-05 21:47:40 +0300

    dynamic calculator in the language "html,css,js" with the help of artificial intelligence

---
+++ calculator_gpt_2/ChatGPT.js
@@ -0,0 +1,10 @@
+function add() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var result = parseFloat(num1) + parseFloat(num2);
+    document.getElementById("result").value = result;
+  }
+  
+  function subtract() {
+    var num1 = document.getElementById("num1").value;
+}
