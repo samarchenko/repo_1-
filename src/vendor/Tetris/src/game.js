e333fd93193ea6529e7cba4c975e782641b32273
samarchenko <anton.samarchenko6@gmail.com>
2023-05-11 00:06:42 +0300

    unfinished business!

--- src/vendor/Tetris/src/game.js
+++ src/vendor/Tetris/src/game.js
@@ -33,4 +33,44 @@ export default class Game{
             [0,0,0],
         ]
     }; 
+    movePieceLeft(){
+        this.activePiece.x -= 1;
+
+        if(this.isPieceOtOfBounds()){
+            this.activePiece.x += 1;  
+        }
+    }
+
+    movePieceRight(){
+        this.activePiece.x += 1;  
+        
+        if(this.isPieceOtOfBounds()){
+            this.activePiece.x -= 1;  
+        }
+    }
+
+    movePieceDown(){
+        this.activePiece.y += 1;   
+
+        if(this.isPieceOtOfBounds()){
+            this.activePiece.y -= 1;  
+        }
+    }
+
+    isPieceOtOfBounds(){
+        const  playfild = this.playfild
+        const { y, x } = this.activePiece; 
+
+        return playfild[y] === undefined || playfild[y][x] === undefined; 
+    }
+
+    lockPiece(){
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;  
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                this.playfild[pieceX + y][pieceX + x] = blocks[y][x]; 
+            }
+        }
+    }
 }
